85e6f7c1c29145fcef97b34e9077735d68802cf6
Terry Hearst <thearst3rd@gmail.com>
2022-10-06 21:48:05 -0400

    Add main menu

---
+++ src/states/Menu.gd
@@ -0,0 +1,14 @@
+extends Control
+
+
+func _ready() -> void:
+	pass
+
+
+func _on_PlayButton_pressed() -> void:
+	var error = get_tree().change_scene("res://src/states/Game.tscn")
+	assert(not error)
+
+
+func _on_QuitButton_pressed() -> void:
+	get_tree().notification(NOTIFICATION_WM_QUIT_REQUEST)
