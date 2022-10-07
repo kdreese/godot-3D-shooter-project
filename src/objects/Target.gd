2a9fcafe262e5798409853f9553aebed69c39092
jokertyf <admoore@wpi.edu>
2022-10-06 20:40:40 -0400

    Minor fixes

--- src/objects/Target.gd
+++ src/objects/Target.gd
@@ -1,10 +1,6 @@
 extends KinematicBody
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
 func on_hit(area: Area) -> void:
 	print("Target got hit.")
 	queue_free()
