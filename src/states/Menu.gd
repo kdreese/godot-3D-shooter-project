714543fafce3da4c68c893af9427830c7bf48199
Terry Hearst <thearst3rd@gmail.com>
2022-10-30 21:19:39 -0400

    Address comments

--- src/states/Menu.gd
+++ src/states/Menu.gd
@@ -1,10 +1,10 @@
 extends Control
 
 
-func _on_PlayButton_pressed() -> void:
+func play() -> void:
 	var error = get_tree().change_scene("res://src/states/Game.tscn")
 	assert(not error)
 
 
-func _on_QuitButton_pressed() -> void:
+func quit_game() -> void:
 	get_tree().notification(NOTIFICATION_WM_QUIT_REQUEST)
--- src/states/PauseMenu.gd
+++ src/states/PauseMenu.gd
@@ -6,12 +6,13 @@ func open_menu() -> void:
 	show()
 
 
-func resume_game() -> void:
+func close_menu() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	hide()
 
 
-func disconnect_from_game() -> void:
+func disconnect_from_server() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	# TODO - put in disconnect code here. Probably we'll emit a signal which tells the Game scene to disconnect
 	var error := get_tree().change_scene("res://src/states/Menu.tscn")
 	assert(not error)
