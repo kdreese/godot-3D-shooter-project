28efacd8849d9bd85a889c572ee2c02607c40fb4
Terry Hearst <thearst3rd@gmail.com>
2022-10-30 22:58:32 -0400

    Initial multiplayer connection functionality

---
+++ src/autoload/MultiplayerInfo.gd
@@ -0,0 +1,56 @@
+extends Node
+
+
+func _ready():
+	var error := get_tree().connect("network_peer_connected", self, "_player_connected")
+	assert(not error)
+	error = get_tree().connect("network_peer_disconnected", self, "_player_disconnected")
+	assert(not error)
+	error = get_tree().connect("connected_to_server", self, "_connected_ok")
+	assert(not error)
+	error = get_tree().connect("connection_failed", self, "_connected_fail")
+	assert(not error)
+	error = get_tree().connect("server_disconnected", self, "_server_disconnected")
+	assert(not error)
+
+
+# Player info, associate ID to data
+var player_info = {}
+# Info we send to other players
+var my_info = { name = "Johnson Magenta", favorite_color = Color8(255, 0, 255) }
+
+
+func _player_connected(id):
+	# Called on both clients and server when a peer connects. Send my info to it.
+	print("Player id %d connected" % [id])
+	rpc_id(id, "register_player", my_info)
+
+
+func _player_disconnected(id):
+	print("Player id %d disconnected" % [id])
+	player_info.erase(id) # Erase player from info.
+
+
+func _connected_ok():
+	print("Connected ok")
+	# Only called on clients, not server. Will go unused; not useful here.
+
+
+func _server_disconnected():
+	print("Server disconnected")
+	pass # Server kicked us; show error and abort.
+
+
+func _connected_fail():
+	print("Connection failed")
+	pass # Could not even connect to server; abort.
+
+
+remote func register_player(info):
+	# Get the id of the RPC sender.
+	var id = get_tree().get_rpc_sender_id()
+	# Store the info
+	player_info[id] = info
+	print("Player %d has info %s" % [id, info])
+
+	# Call function to update lobby UI here
--- src/states/Menu.gd
+++ src/states/Menu.gd
@@ -1,9 +1,30 @@
 extends Control
 
 
+onready var name_line_edit := $"%NameLineEdit" as LineEdit
+onready var address_line_edit := $"%IpLineEdit" as LineEdit
+onready var port_spin_box := $"%PortSpinBox" as SpinBox
+
+
 func play() -> void:
 	var error = get_tree().change_scene("res://src/states/Game.tscn")
 	assert(not error)
+
+
+func host_session() -> void:
+	MultiplayerInfo.my_info.name = name_line_edit.text
+	var peer = NetworkedMultiplayerENet.new()
+	peer.create_server(port_spin_box.value, 4)
+	get_tree().network_peer = peer
+	play()
+
+
+func join_session() -> void:
+	MultiplayerInfo.my_info.name = name_line_edit.text
+	var peer = NetworkedMultiplayerENet.new()
+	peer.create_client(address_line_edit.text, port_spin_box.value)
+	get_tree().network_peer = peer
+	play()
 
 
 func quit_game() -> void:
