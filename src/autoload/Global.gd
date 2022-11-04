a5017db1f8955ec8afe7013b6b32163aae0deeda
Terry Hearst <thearst3rd@gmail.com>
2022-11-03 21:51:37 -0400

    Keyboard shortcut to toggle fullscreen

---
+++ src/autoload/Global.gd
@@ -0,0 +1,11 @@
+extends Node
+
+
+func _ready() -> void:
+	randomize()
+
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("toggle_fullscreen"):
+		OS.window_fullscreen = not OS.window_fullscreen
+		get_tree().set_input_as_handled()
