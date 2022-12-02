3ced3f001a39c9595c527a0afd2e9f8f81d12f9f
jokertyf <admoore@wpi.edu>
2022-12-01 23:17:41 -0500

    Made targets sync better.

--- src/objects/Player.gd
+++ src/objects/Player.gd
@@ -124,5 +124,5 @@ func shoot():
 	if hitscan.is_colliding():
 		var hit := hitscan.get_collider()
 		if hit.has_method("on_raycast_hit"):
-			hit.on_raycast_hit()
+			hit.on_raycast_hit(get_tree().get_network_unique_id())
 	hitscan.set_enabled(false)
