cb6dc3a94eceb20ae373742ffe9d0fadc8e0456a
jokertyf <admoore@wpi.edu>
2023-01-19 21:54:29 -0500

    Added mouse sensitivity slider.

--- src/states/PauseMenu.gd
+++ src/states/PauseMenu.gd
@@ -2,13 +2,14 @@ extends ColorRect
 
 
 onready var disconnect_button := $"%DisconnectButton" as Button
-
+onready var mouse_sens_slider := $"%MouseSensSlider" as HSlider
 
 func _ready() -> void:
 	if get_tree().is_network_server():
 		disconnect_button.text = "Stop Hosting"
 	if not get_tree().network_peer:
 		disconnect_button.text = "Quit Free Play"
+	mouse_sens_slider.value = Global.config["mouse_sensitivity"]
 
 
 func open_menu() -> void:
@@ -23,3 +24,7 @@ func close_menu() -> void:
 
 func disconnect_from_server() -> void:
 	Multiplayer.disconnect_from_session()
+
+
+func on_mouse_sens_change(value: float) -> void:
+	Global.config["mouse_sensitivity"] = value
