d6b684aaa5decc33e6a492bf298a4aabc06011b7
Terry Hearst <thearst3rd@gmail.com>
2023-02-09 22:23:45 -0500

    Refactor the color button circles to be more consistent

--- src/objects/ColorButton.gd
+++ src/objects/ColorButton.gd
@@ -1,4 +1,4 @@
-extends Button
+extends CenterContainer
 class_name ColorButton
 
 
@@ -7,47 +7,41 @@ const BUTTON_CIRCLE_RADIUS_SCALE: float = 0.8
 const HOVER_SCALE: float = 1.1
 const PRESSED_SCALE: float = 1.05
 
+onready var button: Button = $"%Button"
+
+var color: Color
+
 
 # A default stylebox for color buttons.
 var stylebox: StyleBox = preload("res://resources/ui_themes/ColorButtonStylebox.tres")
-# The angle for this button from vertical.
-var angle: float = 0.0
 
 
 # Connect signals to set button size.
 func _ready() -> void:
-	connect("mouse_entered", self, "set_size_info", [HOVER_SCALE])
-	connect("mouse_exited", self, "set_size_info", [1.0])
+	button.connect("mouse_entered", self, "set_button_size", [HOVER_SCALE])
+	button.connect("mouse_exited", self, "set_button_size", [1.0])
 
-
-# Set the initial color of the button.
-# :param color: The color of this button (when not disabled)
-# :param angle: The angle from vertical to this button within the button circle.
-func init(color: Color, init_angle: float) -> void:
-	angle = init_angle
 	var new_stylebox := stylebox.duplicate()
 	new_stylebox.bg_color = color
-	add_stylebox_override("normal", new_stylebox)
-	add_stylebox_override("focus", new_stylebox)
-	add_stylebox_override("pressed", new_stylebox)
-	add_stylebox_override("hover", new_stylebox)
+	button.add_stylebox_override("normal", new_stylebox)
+	button.add_stylebox_override("focus", new_stylebox)
+	button.add_stylebox_override("pressed", new_stylebox)
+	button.add_stylebox_override("hover", new_stylebox)
 	var disabled_stylebox := stylebox.duplicate()
 	disabled_stylebox.bg_color = color.darkened(0.666) # 😈
-	add_stylebox_override("disabled", disabled_stylebox)
-	set_size_info(1.0)
+	button.add_stylebox_override("disabled", disabled_stylebox)
+	set_button_size(1.0)
+
+
+# Sets the color of this button. Make sure this function is called BEFORE _ready
+# :param color: The color of this button (when not disabled)
+func set_button_color(new_color: Color) -> void:
+	color = new_color
 
 
 # Set the scale of the button, while preserving the center position.
 # :param scale: The scale to set this button to.
-func set_size_info(scale: float) -> void:
-	var true_scale := 1.0 if disabled else scale
-	# Get the position of the center of the button circle.
-	var button_circle := get_parent() as Control
-	var circle_center_position := button_circle.rect_global_position + 0.5 * button_circle.rect_size
-	# The radius of the circle should be slightly less than half the maximum allowed by the rectangle
-	var min_rect_dimension := min(button_circle.rect_size.x, button_circle.rect_size.y)
-	var circle_radius := 0.5 * BUTTON_CIRCLE_RADIUS_SCALE * min_rect_dimension
-	# The center position of the button
-	var button_center_position := circle_center_position + circle_radius * Vector2(sin(angle), -cos(angle))
-	rect_size = 2 * BUTTON_RADIUS * true_scale * Vector2(1.0, 1.0)
-	rect_global_position = button_center_position - BUTTON_RADIUS * true_scale * Vector2(1.0, 1.0)
+func set_button_size(scale: float) -> void:
+	var true_scale := 1.0 if button.disabled else scale
+	button.rect_min_size = 2.0 * BUTTON_RADIUS * true_scale * Vector2.ONE
+	print("Setting button size: %.2f" % scale)
