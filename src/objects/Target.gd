30a2ba47f4c2e01bdad7a5f3024b9b7f20be766d
Kyle Reese <reesekyle1998@gmail.com>
2023-02-09 20:43:08 -0500

    Arrows now interact with targets

--- src/objects/Target.gd
+++ src/objects/Target.gd
@@ -12,6 +12,13 @@ func on_raycast_hit(player_id: int) -> void:
 
 
 remote func destroy_self(player_id: int) -> void:
-	emit_signal("target_destroyed", player_id)
-	get_parent().remove_child(self)
+	get_parent().call_deferred("remove_child", self)
 	queue_free()
+	emit_signal("target_destroyed", player_id)
+
+
+func _on_Target_body_entered(body: Node) -> void:
+	if body.is_in_group("Arrow"):
+		if get_tree().has_network_peer():
+			rpc("destroy_self", int(body.archer.name))
+		destroy_self(int(body.archer.name))
